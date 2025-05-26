e45fb6ede92a3d71ff4cafb36817fa54b613534f
bebohany644546 <hanyp878@gmail.com>
2025-05-26 03:26:17 -0700

    Fix configuration errors

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
