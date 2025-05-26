8bfc872d38d78041018d06d772449afabb40a2df
bebohany644546 <hanyp878@gmail.com>
2025-05-26 03:27:54 -0700

    Create main entry point file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-center p-4">تطبيق الفيزياء</h1>
+    </div>
+  )
+}
+
+export default App
